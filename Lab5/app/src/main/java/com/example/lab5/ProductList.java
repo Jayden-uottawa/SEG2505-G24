4f5075606095a1acb3019b2340b6e37a38b3ff98
Jayden Bergevin <Jberg114@uottawa.ca>
2021-11-04 19:11:53 -0400

    Changes and added the class for lab 6

--- Lab5/app/src/main/java/com/example/lab5/ProductList.java
+++ Lab5/app/src/main/java/com/example/lab5/ProductList.java
@@ -5,6 +5,7 @@ package com.example.lab5;
  * Created by Miguel Garzón on 2017-05-09.
  */
 import android.app.Activity;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +19,14 @@ public class ProductList extends ArrayAdapter<Product> {
     List<Product> products;
 
     public ProductList(Activity context, List<Product> products) {
-        super(context, R.layout.support_simple_spinner_dropdown_item, products);
+        super(context, R.layout.layout_product_list, products);
         this.context = context;
         this.products = products;
     }
 
-    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
-        View listViewItem = inflater.inflate(R.layout.support_simple_spinner_dropdown_item, null, true);
+        View listViewItem = inflater.inflate(R.layout.layout_product_list, null, true);
 
         TextView textViewName = (TextView) listViewItem.findViewById(R.id.editTextName);
         TextView textViewPrice = (TextView) listViewItem.findViewById(R.id.editTextPrice);
