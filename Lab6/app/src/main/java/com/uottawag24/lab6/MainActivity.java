d47ec634bfdc85444e3117a4ee62f73c60eca18e
Jayden Bergevin <Jberg114@uottawa.ca>
2021-11-05 12:09:40 -0400

    Worked on lab 6 code

--- Lab6/app/src/main/java/com/uottawag24/lab6/MainActivity.java
+++ Lab6/app/src/main/java/com/uottawag24/lab6/MainActivity.java
@@ -2,13 +2,47 @@ package com.uottawag24.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView idView;
+    EditText productBox;
+    EditText skuBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        skuBox = (EditText) findViewById(R.id.productSku);
+    }
+
+    public void newProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        Product product = dbHandler.findProduct(productBox.getText().toString());
+
+    }
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        Product product = dbHandler.findProduct(productBox.getText().toString());
+
+    }
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler = new MyDBHandler(this);
+        boolean result = dbHandler.deleteProduct(productBox.getText().toString());
+
+    }
+
+    public void about (View view) {
+        Intent aboutIntent = new Intent(this, About.class);
+        startActivity(aboutIntent);
     }
 }
