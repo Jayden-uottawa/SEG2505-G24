6e157e3dd44be9cf7cf2ffafefdd5d89c3200a84
KariganBoudreau <71849802+KariganBoudreau@users.noreply.github.com>
2021-11-11 19:54:28 -0500

    Fichiers Lab6
    Ajout des fichiers du Lab 6 et le test de base de JUnit (incomplet)

---
+++ Lab7/app/src/androidTest/java/com/example/seg2105/labtesting/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.seg2105.labtesting;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    private MainActivity mActivity = null;
+    private TextView text;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    @UiThreadTest
+    public void checkFirstName() throws Exception {
+        assertNotNull(mActivity.findViewById(R.id.textView1));
+        text = mActivity.findViewById(R.id.username);
+        text.setText("user1");
+        String name = text.getText().toString();
+        assertNotEquals("user", name);
+    }
+}
---
+++ Lab7/app/src/main/java/com/example/seg2105/labtesting/ActivityTestRule.java
@@ -0,0 +1,10 @@
+package com.example.seg2105.labtesting;
+
+public class ActivityTestRule<T> {
+    public ActivityTestRule(Class<T> mainActivityClass) {
+        
+    }
+
+    public T getActivity() {
+    }
+}
